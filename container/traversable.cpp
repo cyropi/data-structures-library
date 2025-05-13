5ee2b61954885be3c657f0ae359d43fd03dcf032
Ciro Pizza <ciropizza2002@gmail.com>
2025-05-13 17:05:35 +0200

    Update traversable.cpp

--- container/traversable.cpp
+++ container/traversable.cpp
@@ -1,84 +1,72 @@
 
-#include "traversable.hpp"
+//#include "traversable.hpp"
+
 
 namespace lasd 
-{
-    /* ************************************************************************** */
-    // (TraversableContainer)
+{    
+    // TraversableContainer
 
     template <typename Data>
     template <typename Accumulator>
     Accumulator TraversableContainer<Data>::Fold(FoldFun<Accumulator> foldFun, Accumulator accumulator) const
     {
-        Traverse
-        (
-            [foldFun, &accumulator] (const Data& data) -> void{ accumulator = foldFun(data, accumulator); }
-        );
+        this->Traverse([foldFun, &accumulator] (const Data& data) 
+                        -> void{ accumulator = foldFun(data, accumulator); });
 
         return accumulator;
-    };
+    }
 
 
     template <typename Data>
-    inline bool TraversableContainer<Data>::Exists(const Data& inputData) const noexcept 
+    bool TraversableContainer<Data>::Exists(const Data& inputData) const noexcept 
     {
         bool isPresent = false;
         
-        Traverse
-        (
-            [inputData, &isPresent] (const Data& containerData) -> void{ if(inputData == containerData) isPresent = true; }
-        );
+        this->Traverse([inputData, &isPresent] (const Data& containerData) 
+                        -> void{ if(inputData == containerData) isPresent = true; });
 
         return isPresent;
-    };
+    }
 
 
 
-    /* ************************************************************************** */
     // PreOrderTraversableContainer
 
     template <typename Data>
     template <typename Accumulator>
     Accumulator PreOrderTraversableContainer<Data>::PreOrderFold(FoldFun<Accumulator> foldFun, Accumulator accumulator) const
     {
-        PreOrderTraverse
-        (
-            [foldFun, &accumulator] (const Data& data) -> void{ accumulator = foldFun(data, accumulator); }
-        );
+        this->PreOrderTraverse([foldFun, &accumulator] (const Data& data) 
+                                -> void{ accumulator = foldFun(data, accumulator); });
 
         return accumulator;
-    };
+    }
 
 
     template <typename Data>
-    inline void PreOrderTraversableContainer<Data>::Traverse(TraverseFun traverseFun) const 
+    void PreOrderTraversableContainer<Data>::Traverse(TraverseFun traverseFun) const 
     {
-        PreOrderTraverse(traverseFun);
-    };
+        this->PreOrderTraverse(traverseFun);
+    }
 
 
 
-    /* ************************************************************************** */
     // PostOrderTraversableContainer
 
     template <typename Data>
     template <typename Accumulator>
     Accumulator PostOrderTraversableContainer<Data>::PostOrderFold(FoldFun<Accumulator> foldFun, Accumulator accumulator) const
     {
-        PostOrderTraverse
-        (
-            [foldFun, &accumulator] (const Data& data) -> void{ accumulator = foldFun(data, accumulator); }
-        );
+        this->PostOrderTraverse([foldFun, &accumulator] (const Data& data) 
+                                 -> void{ accumulator = foldFun(data, accumulator); });
 
         return accumulator;
-    };
+    }
 
 
     template <typename Data>
-    inline void PostOrderTraversableContainer<Data>::Traverse(TraverseFun traverseFun) const 
+    void PostOrderTraversableContainer<Data>::Traverse(TraverseFun traverseFun) const 
     {
-        PostOrderTraverse(traverseFun);
-    };
-
-    /* ************************************************************************** */
+        this->PostOrderTraverse(traverseFun);
+    }
 }
