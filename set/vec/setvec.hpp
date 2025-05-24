4b15bd6c84fa62eea79bc8f36452b72f2063efd9
Ciro Pizza <ciropizza2002@gmail.com>
2025-05-24 17:49:13 +0200

    Update setvec.hpp

--- set/vec/setvec.hpp
+++ set/vec/setvec.hpp
@@ -1,1 +1,124 @@
 
+
+#ifndef SETVEC_HPP
+#define SETVEC_HPP
+
+#include "../set.hpp"
+#include "../../vector/vector.hpp"
+
+
+
+namespace lasd 
+{
+    template <typename Data>
+    class SetVec : virtual public Set<Data>,
+                   virtual public ResizableContainer
+    {
+        private:
+            // ...
+
+        protected:
+            //using Container::size;
+            Vector<Data> vector; 
+            ulong head = 0;
+            ulong tail = 0; 
+            ulong capacity = 0;
+            //static const ulong DEFAULT_SIZE = 10;
+
+
+        public:
+            // Default constructor
+            SetVec() = default;
+            //SetVec(ulong);
+
+
+            // Specific constructors
+            SetVec(const TraversableContainer<Data>&); // A set obtained from a TraversableContainer
+            SetVec(MappableContainer<Data>&&); // A set obtained from a MappableContainer
+
+
+            // Copy constructor
+            SetVec(const SetVec<Data>&);
+
+            // Move constructor
+            SetVec(SetVec<Data>&&) noexcept;
+
+
+            // Destructor
+            virtual ~SetVec();
+
+
+            // Copy assignment
+            SetVec<Data>& operator=(const SetVec<Data>&);
+
+            // Move assignment
+            SetVec<Data>& operator=(SetVec<Data>&&) noexcept;
+
+
+            // Comparison operators
+            bool operator==(const SetVec<Data>&) const noexcept;
+            bool operator!=(const SetVec<Data>&) const noexcept;
+
+
+            // Specific member functions (inherited from OrderedDictionaryContainer)
+			const Data& Min() const noexcept(false) override; // Override OrderedDictionaryContainer member (concrete function must throw std::length_error when empty)
+			void RemoveMin() noexcept(false) override; // Override OrderedDictionaryContainer member (concrete function must throw std::length_error when empty)
+			Data MinNRemove() noexcept(false) override; // Override OrderedDictionaryContainer member (concrete function must throw std::length_error when empty)
+
+			const Data& Max() const noexcept(false) override; // Override OrderedDictionaryContainer member (concrete function must throw std::length_error when empty)
+			void RemoveMax() noexcept(false) override; // Override OrderedDictionaryContainer member (concrete function must throw std::length_error when empty)
+			Data MaxNRemove() noexcept(false) override; // Override OrderedDictionaryContainer member (concrete function must throw std::length_error when empty)
+
+			const Data& Predecessor(const Data&) const noexcept(false) override; // Override OrderedDictionaryContainer member (concrete function must throw std::length_error when not found)
+			void RemovePredecessor(const Data&) noexcept(false) override; // Override OrderedDictionaryContainer member (concrete function must throw std::length_error when not found)
+			Data PredecessorNRemove(const Data&) noexcept(false) override; // Override OrderedDictionaryContainer member (concrete function must throw std::length_error when not found)
+
+			const Data& Successor(const Data&) const noexcept(false) override; // Override OrderedDictionaryContainer member (concrete function must throw std::length_error when not found)
+			void RemoveSuccessor(const Data&) noexcept(false) override; // Override OrderedDictionaryContainer member (concrete function must throw std::length_error when not found)
+			Data SuccessorNRemove(const Data&) noexcept(false) override; // Override OrderedDictionaryContainer member (concrete function must throw std::length_error when not found)
+
+
+			// Specific member functions (inherited from DictionaryContainer)
+			bool Insert(const Data&) override; // Override DictionaryContainer member (copy of the value)
+			bool Insert(Data&&) override; // Override DictionaryContainer member (move of the value)
+			bool Remove(const Data&) override; // Override DictionaryContainer member
+
+
+			// Specific member functions (inherited from LinearContainer)
+			const Data& operator[](ulong) const noexcept(false) override; // Override LinearContainer member (must throw std::out_of_range when out of range)
+
+
+			// Specific member function (inherited from TestableContainer)
+			bool Exists(const Data&) const noexcept override; // Override TestableContainer member
+
+
+			// Specific member function (inherited from ClearableContainer)
+			void Clear() override; // Override ClearableContainer member
+
+
+			// Specific member function (inherited from ResizableContainer)
+			void Resize(ulong) override; // Override ResizableContainer member
+
+            void getInfo()
+            {
+                std::cout << "\n\nhead: " << this->head; 
+                std::cout << "\n\ntail: " << this->tail; 
+                std::cout << "\n\nsize: " << this->size; 
+                std::cout << "\n\ncapacity: " << this->Capacity(); 
+            }
+
+
+        protected:
+            virtual ulong Capacity() const noexcept;
+            virtual ulong BinarySearch(const Data&) const noexcept(false);
+            virtual ulong BinarySearchLowerBound(const Data&) const noexcept(false);
+            virtual ulong BinarySearchPredecessor(const Data&) const noexcept(false);
+            virtual ulong BinarySearchSuccessor(const Data&) const noexcept(false);
+
+            // Auxiliary functions, if necessary!
+    };
+}
+
+#include "setvec.cpp"
+
+#endif
