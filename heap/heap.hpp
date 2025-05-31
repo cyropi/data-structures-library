889eec160057527ee8e9d396814895e356429d23
Ciro Pizza <ciropizza2002@gmail.com>
2025-05-31 19:04:46 +0200

    Update heap.hpp

--- heap/heap.hpp
+++ heap/heap.hpp
@@ -1,1 +1,35 @@
 
+#ifndef HEAP_HPP
+#define HEAP_HPP
+
+#include "../container/linear.hpp"
+
+
+namespace lasd 
+{
+    template <typename Data>
+    class Heap : virtual public SortableLinearContainer<Data>,
+                 virtual public ClearableContainer
+    {
+        private:
+
+        protected:
+
+        public:
+            // Destructor
+            virtual ~Heap() = default;
+
+
+            // Copy assignment
+            Heap& operator=(const Heap&) = delete; // Copy assignment of abstract types is not possible.
+            // Move assignment
+            Heap& operator=(Heap&&) noexcept = delete;  // Move assignment of abstract types is not possible.
+
+
+            // Specific member functions
+            virtual bool IsHeap() const = 0;
+            virtual void Heapify() = 0;
+    };
+}
+
+#endif
