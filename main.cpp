385bf8d667d645faced9b0a26a27a703b3e29bba
Ciro Pizza <ciropizza2002@gmail.com>
2025-06-09 22:45:49 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,18 +1,1 @@
 
-#include "zlasdtest/test.hpp"
-
-#include "zmytest/test.hpp"
-
-/* ************************************************************************** */
-
-#include <iostream>
-
-/* ************************************************************************** */
-
-int main() 
-{
-	std::cout << "LASD Libraries 2025" << std::endl;
-	lasdtest();
-  	return 0;
-}
-
